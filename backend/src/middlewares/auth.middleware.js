772f67b9b7fdddd571b64b61600ee81fbe42886c
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2024-10-19 16:26:51 +0530

    Update auth.middleware.js

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -8,8 +8,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
-    console.log(token);
+    console.log(req.header);
     if (!token) {
       throw new ApiError(401, "Not authenticated");
     }
